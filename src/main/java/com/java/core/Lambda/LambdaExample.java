d35ff515d7fe4570f5459ce7dc05ce148ff8b5d6
TAACHRA0 <rahul.choudhary@swisscom.com>
2022-12-06 22:00:29 +0100

    start lambda

---
+++ src/main/java/com/java/core/Lambda/LambdaExample.java
@@ -0,0 +1,15 @@
+package com.java.core.Lambda;
+
+/**
+ * Why Lambda
+ * Enables functional programming
+ * Readable and concise code
+ * Easier to use API and library
+ * Enable support for parallel processing
+ *
+ * Problem in Object-Oriented Programming
+ *
+ *
+ */
+public class LambdaExample {
+}
